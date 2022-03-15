6ea7572e6fc79f6ae768ba48ee50b5cfb7430428
AlvaroJorge <alv.jor96@gmail.com>
2022-03-15 16:03:12 -0300

    Scroll animation

--- script.js
+++ script.js
@@ -139,10 +139,22 @@ animate();
 //------------------------------
 
 //Navbar
-
 $(document).ready(function(){
     $('.btn').click(function(){
         $('.item').toggleClass('show');
         $('ul li').toggleClass('hide');
     })
 });
+
+//Navbar Scroll Hide
+let prevScrollpos = window.pageYOffset;
+window.onscroll = () => {
+    let navbar_scroll = document.getElementById('navbar');
+    let currentScrollPos = window.pageYOffset;
+    if(prevScrollpos > currentScrollPos){
+        navbar_scroll.style.top = '0';
+    } else {
+        navbar_scroll.style.top = '-78px';
+    }
+    prevScrollpos = currentScrollPos;
+}
